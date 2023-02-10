c7e53e0be83ffb3392403cf23bbbb6cc554df92f
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-02-10 21:34:40 +0000

    dog-photo-gallery completed

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,1 +1,23 @@
+const showButton = document.getElementById("show-image");
+const disappearButton = document.getElementById("disappear-image");
+const unorderedList = document.querySelector("ul");
 
+showButton.addEventListener("click", async function getRandomDog() {
+  try {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await response.json();
+
+    const listItem = document.createElement("li");
+    const image = document.createElement("img");
+    image.src = data.message;
+
+    listItem.appendChild(image);
+    unorderedList.appendChild(listItem);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+disappearButton.addEventListener("click", function () {
+  unorderedList.innerHTML = "";
+});
